287464d64242360147d76c344ab2c0d9c699da9e
evgengapanovich <evgengapanovich@gmail.com>
2020-05-29 02:00:32 +0300

    fixed scripts bugs in the folder upgrade001

--- MMF_9_9/upgrade/upgrade001/main.sql
+++ MMF_9_9/upgrade/upgrade001/main.sql
@@ -11,21 +11,22 @@ connect &&USER_NAME/oracle
 
 PROMPT Creating tables
 
-@create_table_coaches.sql &&USER_NAME
-@create_table_coaching.sql &&USER_NAME
-@create_table_leagues.sql &&USER_NAME
-@create_table_matches.sql &&USER_NAME
-@create_table_player_statistics.sql &&USER_NAME
-@create_table_players.sql &&USER_NAME
-@create_table_seasons.sql &&USER_NAME
-@create_table_squad.sql &&USER_NAME
-@create_table_stadiums.sql &&USER_NAME
-@create_table_team_statistics.sql &&USER_NAME
-@create_table_teams.sql &&USER_NAME
-@create_table_transfers.sql &&USER_NAME
+@create_table_coaches.sql
+@create_table_coaching.sql
+@create_table_leagues.sql
+@create_table_matches.sql
+@create_table_player_statistics.sql
+@create_table_players.sql
+@create_table_seasons.sql
+@create_table_squad.sql
+@create_table_stadiums.sql
+@create_table_team_statistics.sql
+@create_table_teams.sql
+@create_table_transfers.sql
+@create_table_events.sql
 
 PROMPT Creating constraints
-@create_constraints.sql &&SCHEMA_NAME
+@create_constraints.sql &&USER_NAME
 
 SET SERVEROUTPUT OFF
 SPOOL OFF
